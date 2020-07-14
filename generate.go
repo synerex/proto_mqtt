b6f3c209e9d14c40916e4eb4bfbf72750ad9a89d
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2020-07-15 00:29:14 +0900

    update package name

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
-package mqtt
-/ dummy go file for generating pb.go
+package proto_mqtt
+
+// dummy go file for generating pb.go
 //go:generate protoc -I . --go_out=paths=source_relative:. mqtt.proto
